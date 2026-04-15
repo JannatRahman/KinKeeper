89c921918955766d3a74cb83491e1fa9459a60f4
JannatRahman <you@example.com>
2026-04-15 20:18:58 +0600

    json data fetching done in the cards

--- src/app/apps/page.jsx
+++ src/app/apps/page.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
+const appsPromise = async function () {
+  const res = await fetch ("http://localhost:3000/data.json");
+  const data = await res.json();
+  return data;
+}
+
 const AppDetailsPage =async ({params}) => {
+ const apps = await appsPromise();
+ console.log(apps, "all apps")
+
   const {id} = await params;
   return (
     <div>

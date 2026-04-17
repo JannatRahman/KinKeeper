debd2f836c930a90c26b14160bf6aaff65c7ec6b
JannatRahman <you@example.com>
2026-04-17 21:13:39 +0600

    loading spinner added

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+  return (
+    <div className='text-center p-20'>
+      <h2 className='text-5xl text-green-800 '>Loading... Please wait..</h2>
+    </div>
+  );
+};
+
+export default loading;
